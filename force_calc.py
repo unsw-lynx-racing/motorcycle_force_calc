72531aad7a1b17a6610c4f87b4df66580a4c35ce
Asher Broadhead <z5767463@ad.unsw.edu.au>
2026-09-19 16:48:35 +1000

    Range of corner radii

--- force_calc.py
+++ force_calc.py
@@ -40,9 +40,9 @@ PMAX = 36 * 10**3  # max motor power
 "rider inputs / variables"
 STEER_ANG = math.radians(1)  # steering angle
 VEL_FORWARD = 30  # forward velocity
-RCURVEREAR = 68.0
+RCURVEREAR = np.linspace(68, 250, NUMTESTS)  # rear wheel path curvature
 """WHEEL_BASE / np.tan(KINSTEER_ANG)"""
-ROLL_ANG = math.atan(VEL_FORWARD**2 / (GRAVITY * RCURVEREAR))
+ROLL_ANG = np.arctan(VEL_FORWARD**2 / (GRAVITY * RCURVEREAR))
 """np.linspace(0, math.pi / 3, NUMTESTS)  # roll angle (rad)"""
 
 beta_dash = CASTER_ANG + np.arctan(
@@ -77,14 +77,13 @@ mu = (
     + FWHEEL_THICKNESS
     - RWHEEL_THICKNESS
 ) / ((c4 + c5) * np.cos(ROLL_ANG))  # driving traction coefficient
-KINSTEER_ANG = 0.01911531850199284
-"""np.arctan(
+KINSTEER_ANG = np.arctan(
     (np.sin(STEER_ANG) * np.cos(CASTER_ANG + mu))
     / (
         np.cos(ROLL_ANG) * (np.cos(STEER_ANG))
         - np.sin(ROLL_ANG) * np.sin(STEER_ANG) * np.sin(CASTER_ANG + mu)
     )
-)"""  # kinematic steering angle
+)  # kinematic steering angle
 x_Pf = (c1 + c2) * np.sin(mu) + (c4 + c5) * np.cos(mu)
 y_Pf = (
     (-(c1 + c2) * np.cos(mu) + (c4 + c5) * np.sin(mu)) * np.sin(ROLL_ANG)
@@ -159,7 +158,8 @@ ssrfnorm, ssrrnorm, ssvmax = ss_rectilinear()
 tra_englim, tra_traclim, tra_wheelielim = trans_rectilinear()
 ssafnorm, ssarnorm, ssaflateral, ssarlateral, freq_cof, rreq_cof = ss_cornering()
 with open("force_calc_results.txt", "w") as f:
-    print(f"Cornering Radius = {RCURVEREAR}", file=f)
+    print(f"Corner Radius = {RCURVEREAR}", file=f)
+    print(f"Roll Angle = {np.degrees(ROLL_ANG)}", file=f)
     print(
         f"Level Free Stand:\n Front Normal Force = \n{lfsfnorm}\n Rear Normal Force = \n{lfsrnorm}",
         file=f,
